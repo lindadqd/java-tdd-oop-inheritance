6dfd72b84934c725cfc5e488cdbb97b71e1e25b7
Linda Do <lindado_98@hotmail.com>
2025-08-12 11:06:54 +0200

    completed 1 and 2

--- src/main/java/com/booleanuk/core/Author.java
+++ src/main/java/com/booleanuk/core/Author.java
@@ -1,7 +1,13 @@
 package com.booleanuk.core;
 
 public class Author {
+    private String name;
+    private String contactInformation;
+    private String website;
 
     public Author(String name, String contactInformation, String website){
+        this.name = name;
+        this.contactInformation = contactInformation;
+        this.website = website;
     }
 }
